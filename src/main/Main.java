c00a1cf2d2573cd1fe86c27d8dfda3fe56946818
marcodestefano <marco.destefano1@stud.unifi.it>
2023-09-22 18:10:42 +0200

    :)

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,10 @@
+package main;
+
+import dao.DatabaseDAO;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        DatabaseDAO databaseDAO = new DatabaseDAO();
+        databaseDAO.connect();
+}
 }

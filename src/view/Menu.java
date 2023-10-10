bce39e040553f78ba2aa2a5f9dfa6a1debf24b1b
marco-vignozzi <marcovignozzi7@gmail.com>
2023-10-10 16:17:40 +0200

    nuovo branch per gestione menu con facade

---
+++ src/view/Menu.java
@@ -0,0 +1,9 @@
+package view;
+
+import controller.Controller;
+
+public abstract class Menu {
+    private Controller controller;
+
+    public abstract void display();
+}
---
+++ src/view/MenuLogin.java
@@ -0,0 +1,15 @@
+package view;
+
+import controller.Controller;
+
+public class MenuLogin {
+    private final Controller controller;
+
+    public MenuLogin(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void display() {
+
+    }
+}
